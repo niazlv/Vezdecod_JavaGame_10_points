c671602911b31e86f67fb1754bc193be8c18e983
Niyaz <niazlv03@gmail.com>
2021-10-23 15:31:14 +0300

    added draw polygon

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -13,12 +13,35 @@ public class main extends Window {
     private final Texture background = Texture.load("background.png");
 
     public main() {
-        super(800, 600, "Game in java (10 points)", true, "Cambria Math", 46);
+        super(800, 600, "Game in java (20 points)", true, "Cambria Math", 46);
     }
+    //Количество углов в многоугольнике
+    int n = 3;
+    //размер многоугольника(радиус от центра)
+    int radius = 200;
+    //толщина стенок многоугольника
+    int thickness = 3;
+
     @Override
     protected void onFrame(double elapsed) {
-
+        //рисуем фон
         canvas.drawTexture(background, 0, 0, width, height);
+        drawPolygon(n,radius,thickness);
+    }
+    private void drawPolygon(int n, int radius, int thickness){
+        //рисуем многоугольник
+        int x,y;
+        int lastx, lasty;
+
+        lastx = (width / 2) + (int)(Math.sin(0) * radius);
+        lasty = (height / 2) + (int)(Math.cos(0) * radius);
+        for(double i = 0; i <= (Math.PI * 2); i+=((Math.PI * 2) / n)) {
+            x = (width / 2) + (int)(Math.sin(i) * radius);
+            y = (height / 2) + (int)(Math.cos(i) * radius);
+            canvas.drawLine(10, lastx, lasty, x, y, thickness);
+            lastx = x;
+            lasty = y;
+        }
     }
 
     public static void main(String[] args) {
